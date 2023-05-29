7a0cb971e71fa84a0b2a9cf91dd6f547ca2240d0
JAPlaisted <jonathan.plaisted@outlook.com>
2023-05-29 03:22:16 -0700

    added media padding

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/nav.jsx'
+import Footer from '../components/footer.jsx'
 
 function Book(props) {
     return (
         <div className='container'>
             <Navbar></Navbar>
             Book
+            <Footer/>
         </div>
     );
 }
