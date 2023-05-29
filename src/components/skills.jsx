c5fd69f50827973e7b81f33532d23bfc6d3f782d
JAPlaisted <jonathan.plaisted@outlook.com>
2023-05-29 08:06:59 -0700

    resized skill icons

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -13,34 +13,34 @@ function skills() {
             <div className='skills'>
                 <div className="row">
                     <div className="media" data-aos="fade-left">
-                        <DeveloperModeIcon className='skill-icon' style={{fontSize: "xx-large"}}/>
+                        <DeveloperModeIcon className='skill-icon' style={{fontSize: "xxx-large"}}/>
                         <h4>Front End Development</h4>
                         <p>As a specialized front-end developer with expertise in React, I thrive on solving complex problems and designing intuitive user interfaces.</p>
                     </div>
                     <div className="media" data-aos="fade-left">
-                        <RemoveRedEyeIcon className='skill-icon' style={{fontSize: "xx-large"}}/>
+                        <RemoveRedEyeIcon className='skill-icon' style={{fontSize: "xxx-large"}}/>
                         <h4>Python</h4>
                         <p>I am innovating ways to leverage Python in the browser through computer vision, to enhance interactivity and user engagement on the web.</p>
                     </div>
                     <div className="media" data-aos="fade-left">
-                        <ScreenshotMonitorIcon className='skill-icon' style={{fontSize: "xx-large"}}/>
+                        <ScreenshotMonitorIcon className='skill-icon' style={{fontSize: "xxx-large"}}/>
                         <h4>Responsive Design</h4>
                         <p>I specialize in designing and developing responsive web applications that seamlessly adapt to all screen sizes, including desktop and mobile devices.</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="media" data-aos="fade-left">
-                        <EqualizerIcon className='skill-icon' style={{fontSize: "xx-large"}}/>
+                        <EqualizerIcon className='skill-icon' style={{fontSize: "xxx-large"}}/>
                         <h4>Data Visualization</h4>
                         <p>I leverage cutting-edge data visualization tools to craft compelling narratives and deliver engaging user experiences.</p>
                     </div>
                     <div className="media" data-aos="fade-left">
-                        <CssIcon className='skill-icon' style={{fontSize: "xx-large"}}/>
+                        <CssIcon className='skill-icon' style={{fontSize: "xxx-large"}}/>
                         <h4>CSS</h4>
                         <p>I firmly believe that custom CSS, when paired with a CSS library, offers a powerful toolset for efficient web development while maintaining an element of uniqueness in the design.</p>
                     </div>
                     <div className="media" data-aos="fade-left">
-                        <SmartToyIcon className='skill-icon' style={{fontSize: "xx-large"}}/>
+                        <SmartToyIcon className='skill-icon' style={{fontSize: "xxx-large"}}/>
                         <h4>Prompt Engineering</h4>
                         <p>As an AI developer, I design robust and reliable prompts that generate consistent results for both image and text generation.</p>
                     </div>
